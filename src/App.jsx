9560c0c6201204927e115fbff7c06922d6a70d8f
mohosin2126 <mohosin212@gmail.com>
2023-10-01 20:13:10 +0800

    data loaded by using useeffect state and set it at usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import Blogs from './Components/Blogs'
 import Header from './Components/Header'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
      <Header></Header>
+     <Blogs></Blogs>
     </>
   )
 }
