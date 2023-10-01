886f8141d72f0b73fbde80d31b1c4eeb26d98d05
mohosin2126 <mohosin212@gmail.com>
2023-10-01 19:25:42 +0800

    header added

---
+++ src/Components/Header.jsx
@@ -0,0 +1,11 @@
+
+
+const Header = () => {
+    return (
+        <div className="mt-20">
+            <h1 className="text-center text-4xl font-bold ">Knowledge <span className="text-">Cafe </span></h1>
+        </div>
+    );
+};
+
+export default Header;
