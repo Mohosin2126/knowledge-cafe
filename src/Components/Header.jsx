545345c681df614f3a8af41c89db339fd379c4d1
mohosin2126 <mohosin212@gmail.com>
2023-10-01 20:22:11 +0800

    header or navbar done

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,8 +2,9 @@
 
 const Header = () => {
     return (
-        <div className="mt-20">
-            <h1 className="text-center text-4xl font-bold ">Knowledge <span className="text-">Cafe </span></h1>
+        <div className=" flex justify-between items-center  max-w-screen-xl mx-auto  px-8 lg:px-24 py-5 my-10 border-b-2">
+            <h1 className=" text-4xl font-bold ">Knowledge <span className="text-">Cafe </span></h1>
+            <img src="https://i.ibb.co/QCGrWGh/profile.png" alt="" />
         </div>
     );
 };
